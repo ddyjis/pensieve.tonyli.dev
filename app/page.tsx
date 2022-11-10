34444c9496b9bd20f5f966d75427cc4fa72d8257
ddyjis <ddyjis@gmail.com>
2022-11-10 15:19:27 +0800

    Add SASS

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <h1>Hello, World</h1>;
+  return (
+    <div className="container">
+      {/* TODO: Banner */}
+      <div className="banner">Banner</div>
+      {/* TODO: Search bar */}
+      <div className="search-bar">Search Bar</div>
+      {/* TODO: History */}
+      <div className="history">History</div>
+    </div>
+  );
 }
