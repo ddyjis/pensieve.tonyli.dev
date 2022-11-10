fb542e4e3f7a9d447a4fd7682b66247cfebc76c2
ddyjis <ddyjis@gmail.com>
2022-11-10 16:04:26 +0800

    Update index styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ export default function Home() {
   return (
     <div className="container">
       <Banner />
-      {/* TODO: Search bar */}
-      <div className="search-bar">Search Bar</div>
       {/* TODO: History */}
       <div className="history">History</div>
     </div>
