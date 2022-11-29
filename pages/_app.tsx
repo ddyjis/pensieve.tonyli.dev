c22e5b1e5275452917e506a321e8f99ffdb512b9
ddyjis <ddyjis@gmail.com>
2022-11-29 00:39:04 -0500

    Add @next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import 'styles/main.scss'
 
+import { Fira_Code, Merriweather, Noto_Serif_HK } from '@next/font/google'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 import Layout from '../components/Layout'
 import { PensieveProvider } from '../components/PensieveContext'
+
+const firaCode = Fira_Code({ weight: ['400', '700'], subsets: ['latin'] })
+const merriweather = Merriweather({ weight: ['400', '700'], subsets: ['latin'] })
+const notoSerifHk = Noto_Serif_HK({ weight: ['400', '700'], subsets: ['chinese-hongkong'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +18,12 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Pensieve</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <style jsx global>{`
+        :root {
+          --font-serif: ${merriweather.style.fontFamily}, ${notoSerifHk.style.fontFamily};
+          --font-monospace: ${firaCode.style.fontFamily};
+        }
+      `}</style>
       <PensieveProvider>
         <Layout>
           <Component {...pageProps} />
