eb55aa7768737d30b17407500949f1599a44a424
ddyjis <ddyjis@gmail.com>
2022-12-24 01:38:01 -0500

    Add PWA

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 const path = require('path')
+const withPwa = require('next-pwa')({
+  dest: 'public',
+})
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPwa({
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-}
+})
 
 module.exports = nextConfig
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default class Document extends NextDocument {
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
           <meta name="apple-mobile-web-app-title" content="Pensieve" />
-          <meta name="description" content="A place for organizing my thoughts" />
+          <meta name="description" content="The second brain that help me do the thinking" />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="theme-color" content="#a78d72" />
@@ -25,7 +25,10 @@ export default class Document extends NextDocument {
           <meta name="twitter:card" content="Notes" />
           <meta name="twitter:url" content="https://notes.tonyli.dev" />
           <meta name="twitter:title" content="Pensieve" />
-          <meta name="twitter:description" content="A place for organizing my thoughts" />
+          <meta
+            name="twitter:description"
+            content="The second brain that help me do the thinking"
+          />
           <meta
             name="twitter:image"
             content="https://notes.tonyli.dev/android-chrome-192x192.png"
@@ -33,7 +36,7 @@ export default class Document extends NextDocument {
           <meta name="twitter:creator" content="@__tony_li_dev__" />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="Pensieve" />
-          <meta property="og:description" content="A place for organizing my thoughts" />
+          <meta property="og:description" content="The second brain that help me do the thinking" />
           <meta property="og:site_name" content="Pensieve" />
           <meta property="og:url" content="https://notes.tonyli.dev" />
           <meta property="og:image" content="https://notes.tonyli.dev/apple-touch-icon.png" />
