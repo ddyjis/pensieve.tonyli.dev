23398d25e67044e3817d82290f52d180e6d8ecb5
ddyjis <ddyjis@gmail.com>
2022-12-24 02:49:07 -0500

    Update note styles

--- components/Frontmatter.tsx
+++ components/Frontmatter.tsx
@@ -1,4 +1,5 @@
 import NextLink from 'next/link'
+import Balancer from 'react-wrap-balancer'
 
 import type { Frontmatter as FrontmatterType } from '../lib/firebase'
 
@@ -10,7 +11,9 @@ interface FrontmatterProps {
 export default function Frontmatter({ frontmatter, filename }: FrontmatterProps) {
   return (
     <header className="note__header">
-      <h1 className="note__title">{frontmatter.title}</h1>
+      <h1 className="note__title">
+        <Balancer ratio={0.5}>{frontmatter.title}</Balancer>
+      </h1>
       <div className="note__aliases">
         {frontmatter.aliases?.map((alias) => (
           <div className="note__alias" key={alias}>
@@ -25,17 +28,19 @@ export default function Frontmatter({ frontmatter, filename }: FrontmatterProps)
           </NextLink>
         ))}
       </div>
-      {filename && (
-        <NextLink
-          className="note__edit_link"
-          href={`${process.env.NEXT_PUBLIC_CONTENT_EDIT_BASE_URL}${filename}.md`}
-          target="_blank"
-        >
-          Edit
-        </NextLink>
-      )}
-      <div className="note__create_time">{frontmatter.created}</div>
-      <div className="note__update_time">{frontmatter.updated}</div>
+      <div className="note__metadata">
+        <div className="note__create_time">{frontmatter.created}</div>
+        {filename && (
+          <NextLink
+            className="note__edit_link"
+            href={`${process.env.NEXT_PUBLIC_CONTENT_EDIT_BASE_URL}${filename}.md`}
+            target="_blank"
+          >
+            Edit
+          </NextLink>
+        )}
+        <div className="note__update_time">{frontmatter.updated}</div>
+      </div>
     </header>
   )
 }
