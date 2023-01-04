531fa833162090bbf54f6ac3da340b3c98459a6c
ddyjis <ddyjis@gmail.com>
2023-01-04 01:14:37 -0500

    Make index page SSR and return random note

---
+++ lib/random.ts
@@ -0,0 +1,1 @@
+export const getSample = <T>(items: T[]): T => items[Math.floor(Math.random() * items.length)]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,61 @@
-import Hero from '../components/Hero'
-import History from '../components/History'
+import { type GetServerSideProps } from 'next'
 
-export default function IndexPage() {
+import Hero from '../components/Hero'
+import Note, { type NoteProps } from '../components/Note'
+import {
+  type NoteId,
+  getBacklinkDetails,
+  getDocumentToken,
+  getFilename,
+  getFrontmatter,
+  getNoteIds,
+} from '../lib/firebase'
+import { getSample } from '../lib/random'
+
+type IndexPageProps = (Partial<NoteProps> & { noteId: null }) | (NoteProps & { noteId: NoteId })
+
+export default function IndexPage({
+  frontmatter,
+  filename,
+  documentToken,
+  backlinkDetails,
+  noteId,
+}: IndexPageProps) {
+  if (!noteId) {
+    return <Hero />
+  }
   return (
-    <div className="index">
-      <Hero />
-      <History />
-    </div>
+    <Note
+      frontmatter={frontmatter}
+      filename={filename}
+      documentToken={documentToken}
+      backlinkDetails={backlinkDetails}
+    />
   )
+}
+
+export const getServerSideProps: GetServerSideProps<IndexPageProps> = async () => {
+  const noteIds = (await getNoteIds()) || []
+  if (!noteIds.length) {
+    return { props: { noteId: null } }
+  }
+
+  const noteId = getSample(noteIds)
+  const frontmatter = await getFrontmatter(noteId)
+  const documentToken = await getDocumentToken(noteId)
+  const filename = await getFilename(noteId)
+
+  if (!frontmatter || !documentToken || !filename) {
+    return { props: { noteId: null } }
+  }
+  const backlinkDetails = await getBacklinkDetails(noteId)
+  return {
+    props: {
+      noteId,
+      frontmatter,
+      documentToken,
+      filename,
+      backlinkDetails,
+    },
+  }
 }
